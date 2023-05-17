d8619ab52d800e9437e7163a8a337d9844b81701
Ada Lee <adawinlee@gmail.com>
2023-05-16 20:57:53 -0400

    all complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,36 +11,58 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    return res.send(JSON.stringify(books, null, 4));
+    let getList = new Promise((resolve,reject) => {
+        resolve(res.send(JSON.stringify(books, null, 4)));
+    })
+    getList.then(() => {
+        console.log("Book list retrieved");
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    return res.send(books[req.params.isbn]);
+    let getISBN = new Promise((resolve,reject) => {
+        resolve(res.send(books[req.params.isbn]));
+    })
+    getISBN.then(() => {
+        console.log("Book details based on ISBN retrieved");
+    })
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let ab = [];
     let len = Object.keys(books).length;
-    for (let i = 1; i < len; i++) {
-        if (books[i].author === req.params.author) {
-            ab.push(books[i]);
-        }
-    };
-    return res.send(ab);
+
+    let getAuthor = new Promise((resolve,reject) => {
+        for (let i = 1; i < len; i++) {
+            if (books[i].author === req.params.author) {
+                ab.push(books[i]);
+            }
+        };
+        resolve(res.send(ab));
+    })
+    getAuthor.then(() => {
+        console.log("Book details based on author retrieved");
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let tb = [];
     let len = Object.keys(books).length;
-    for (let i = 1; i < len; i++) {
-        if (books[i].title === req.params.title) {
-            tb.push(books[i]);
-        }
-    };
-    return res.send(tb);
+
+    let getTitle = new Promise((resolve,reject) => {
+        for (let i = 1; i < len; i++) {
+            if (books[i].title === req.params.title) {
+                tb.push(books[i]);
+            }
+        };
+        resolve(res.send(tb));
+    })
+    getTitle.then(() => {
+        console.log("Book details based on title retrieved");
+    })
 });
 
 //  Get book review
